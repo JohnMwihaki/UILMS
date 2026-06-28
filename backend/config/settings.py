008128a7c17abe2526803c3a1810d8d1db284415
john mwihaki <kimanimw21@gmail.com>
2026-06-29 00:44:49 +0300

    solve cors error

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -291,6 +291,10 @@ CORS_ALLOWED_ORIGINS = config(
     cast=lambda v: [origin.strip() for origin in v.split(",")]
 )
 
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    r"^https://.*\.vercel\.app$",
+]
+
 CORS_ALLOW_CREDENTIALS = True
 
 # ==========================
